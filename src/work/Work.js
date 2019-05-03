e790c939f6381cbec0a8fc99f92f122304c95a14
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-03 23:57:35 +0300

    Update Globuzzer exp

--- src/work/Work.js
+++ src/work/Work.js
@@ -10,39 +10,47 @@ export default class Work extends React.Component{
             <div className='work-container'>
                 <div className='exp-holder'>
                     <div className='company-logo-holder'>
-                        <div className='vamk-logo-image-holder'>
+                        <div className='globuzzer-logo-image-holder'>
 
                         </div>
                     </div>
                     <div className='right-info-holder'>
                         <div className='position-holder'>
-                            <p>Front End/Core Developer</p>
+                            <p>Full Stack Developer</p>
                         </div>
                     </div>
 
                     <div className='stack-under-holder'>
-                        <div className='react-holder'>
+                        <div className='stack-holder'>
                             <p>React</p>
                         </div>  
 
-                        <div className='javascript-holder'>
+                        <div className='stack-holder'>
                             <p>Javascript</p>
                         </div>
 
-                        <div className='html-holder'>
+                        <div className='stack-holder'>
                             <p>HTML</p>
                         </div>
 
-                        <div className='css-holder'>
+                        <div className='stack-holder'>
                             <p>CSS</p>
                         </div>
 
-                        <div className='threejs-holder'>
-                            <p>Threejs</p>
+                        <div className='stack-holder'>
+                            <p>Firebase</p>
+                        </div>
+
+                        <div className='stack-holder'>
+                            <p>Stripe APIs</p>
+                        </div>
+
+                        <div className='stack-holder'>
+                            <p>Google Places &amp; Maps APIs</p>
                         </div>
                     </div>
                 </div>
-
+                
                 <div className='exp-holder'>
                     <div className='company-logo-holder'>
                         <div className='vamk-logo-image-holder'>
@@ -56,27 +64,29 @@ export default class Work extends React.Component{
                     </div>
 
                     <div className='stack-under-holder'>
-                        <div className='react-holder'>
+                        <div className='stack-holder'>
                             <p>React</p>
                         </div>  
 
-                        <div className='javascript-holder'>
+                        <div className='stack-holder'>
                             <p>Javascript</p>
                         </div>
 
-                        <div className='html-holder'>
+                        <div className='stack-holder'>
                             <p>HTML</p>
                         </div>
 
-                        <div className='css-holder'>
+                        <div className='stack-holder'>
                             <p>CSS</p>
                         </div>
 
-                        <div className='threejs-holder'>
+                        <div className='stack-holder'>
                             <p>Threejs</p>
                         </div>
                     </div>
                 </div>
+
+                
             </div>
             </>
         )
