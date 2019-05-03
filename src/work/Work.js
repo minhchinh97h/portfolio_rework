98cdd89441b4ba0241dba655276fd069f58502a6
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-03 23:40:26 +0300

    Update vamk exp

--- src/work/Work.js
+++ src/work/Work.js
@@ -10,7 +10,7 @@ export default class Work extends React.Component{
             <div className='work-container'>
                 <div className='exp-holder'>
                     <div className='company-logo-holder'>
-                        <div className='company-logo-image-holder'>
+                        <div className='vamk-logo-image-holder'>
 
                         </div>
                     </div>
@@ -21,7 +21,60 @@ export default class Work extends React.Component{
                     </div>
 
                     <div className='stack-under-holder'>
+                        <div className='react-holder'>
+                            <p>React</p>
+                        </div>  
 
+                        <div className='javascript-holder'>
+                            <p>Javascript</p>
+                        </div>
+
+                        <div className='html-holder'>
+                            <p>HTML</p>
+                        </div>
+
+                        <div className='css-holder'>
+                            <p>CSS</p>
+                        </div>
+
+                        <div className='threejs-holder'>
+                            <p>Threejs</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='exp-holder'>
+                    <div className='company-logo-holder'>
+                        <div className='vamk-logo-image-holder'>
+
+                        </div>
+                    </div>
+                    <div className='right-info-holder'>
+                        <div className='position-holder'>
+                            <p>Front End/Core Developer</p>
+                        </div>
+                    </div>
+
+                    <div className='stack-under-holder'>
+                        <div className='react-holder'>
+                            <p>React</p>
+                        </div>  
+
+                        <div className='javascript-holder'>
+                            <p>Javascript</p>
+                        </div>
+
+                        <div className='html-holder'>
+                            <p>HTML</p>
+                        </div>
+
+                        <div className='css-holder'>
+                            <p>CSS</p>
+                        </div>
+
+                        <div className='threejs-holder'>
+                            <p>Threejs</p>
+                        </div>
                     </div>
                 </div>
             </div>
