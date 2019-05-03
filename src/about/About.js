aad48f6708fc5f5c82211665baa5452e7af875fb
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-03 22:38:32 +0300

    Update new fonts

--- src/about/About.js
+++ src/about/About.js
@@ -5,13 +5,15 @@ export default class About extends React.Component{
 
     render(){
         return(
-            <div className='about-container'> 
-                <div className='text-holder'>
-                    <span>
-                        <p>Curious, Ambitious and Devoted are three words that precisely describe my attitude towards coding. 
-                        I have 1+ year professional experience in web development, with specialization of React, Javascript, HTML 5, CSS and NoSQL database such as MongoDB, Firebase. 
-                        When facing any challenges, I urge myself to solve the issues at once. I am only pleased when everything is done and perfect.</p>
-                    </span>
+            <div className='about-container'>
+                <div className='content-holder'>
+                    <div className='text-holder'>
+                        <span>
+                            <p>&ldquo;Curious, Ambitious and Devoted are three words that precisely describe my attitude towards coding. 
+                            I have 1+ year professional experience in web development, with specialization of React, Javascript, HTML 5, CSS and NoSQL database such as MongoDB, Firebase. 
+                            When facing any challenges, I urge myself to solve the issues at once. I am only pleased when everything is done and perfect.&rdquo;</p>
+                        </span>
+                    </div>
                 </div>
             </div>
         )
