bd45a774449f8e5f01cb48f7b197bbda702ada3d
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-04 16:12:25 +0300

    Update wwo exp

--- src/work/Work.js
+++ src/work/Work.js
@@ -17,6 +17,9 @@ export default class Work extends React.Component{
                     <div className='right-info-holder'>
                         <div className='position-holder'>
                             <p>Full Stack Developer</p>
+                            <span><p className="position-smaller-text">
+                                The role is to build, develop and test new/old features for e-commercial websites.
+                            </p></span>
                         </div>
                     </div>
 
@@ -53,6 +56,45 @@ export default class Work extends React.Component{
 
                 <div className='exp-holder'>
                     <div className='company-logo-holder'>
+                        <div className='wwo-logo-image-holder'>
+
+                        </div>
+                    </div>
+
+                    <div className='right-info-holder'>
+                        <div className='position-holder'>
+                            <p>Personal Project</p>
+                            <span><p className="position-smaller-text">
+                                &ldquo;Werewolves Online&rdquo; is an online web application focusing on helping people play 'The werewolves of Millers Hollow' without any actual physical contacts.
+                            </p></span>
+                        </div>
+                    </div>
+
+                    <div className='stack-under-holder'>
+                        <div className='stack-holder'>
+                            <p>React/React Router</p>
+                        </div>  
+                        
+                        <div className='stack-holder'>
+                            <p>MongoDB/Mongoose/Mlab</p>
+                        </div>
+
+                        <div className='stack-holder'>
+                            <p>Javascript</p>
+                        </div>
+
+                        <div className='stack-holder'>
+                            <p>HTML</p>
+                        </div>
+
+                        <div className='stack-holder'>
+                            <p>CSS</p>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className='exp-holder'>
+                    <div className='company-logo-holder'>
                         <div className='vamk-logo-image-holder'>
 
                         </div>
@@ -60,6 +102,9 @@ export default class Work extends React.Component{
                     <div className='right-info-holder'>
                         <div className='position-holder'>
                             <p>Front End/Core Developer</p>
+                            <span><p className="position-smaller-text">
+                                In charge of building and developing core 3D functionalities with Threejs framework for the project 'Configurator 3D'.
+                            </p></span>
                         </div>
                     </div>
 
@@ -95,6 +140,9 @@ export default class Work extends React.Component{
                     <div className='right-info-holder'>
                         <div className='position-holder'>
                             <p>IoT Software Developer</p>
+                            <span><p className="position-smaller-text">
+                                Responsible for developing a IoT web application associating with MindSphere's Cloud Platform.
+                            </p></span>
                         </div>
                     </div>
 
@@ -116,45 +164,12 @@ export default class Work extends React.Component{
                         </div>
 
                         <div className='stack-holder'>
-                            <p>Mindsphere APIs</p>
+                            <p>MindSphere APIs</p>
                         </div>
                     </div>
                 </div>
 
-                <div className='exp-holder'>
-                    <div className='company-logo-holder'>
-                        <div className='fpt-logo-image-holder'>
-
-                        </div>
-                    </div>
-                    <div className='right-info-holder'>
-                        <div className='position-holder'>
-                            <p>IoT Software Developer</p>
-                        </div>
-                    </div>
-
-                    <div className='stack-under-holder'>
-                        <div className='stack-holder'>
-                            <p>Angularjs</p>
-                        </div>  
-
-                        <div className='stack-holder'>
-                            <p>Javascript</p>
-                        </div>
-
-                        <div className='stack-holder'>
-                            <p>HTML</p>
-                        </div>
-
-                        <div className='stack-holder'>
-                            <p>CSS</p>
-                        </div>
-
-                        <div className='stack-holder'>
-                            <p>Mindsphere APIs</p>
-                        </div>
-                    </div>
-                </div>
+                
 
             </div>
             </>
