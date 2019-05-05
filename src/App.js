7177fb2d8bafa1721d70d457c554e0fc927a55fb
minhchinh97h <minhchinhduong97@gmail.com>
2019-05-05 17:46:54 +0300

    Update contact

--- src/App.js
+++ src/App.js
@@ -5,20 +5,31 @@ import Header from './header/Header'
 import Intro from './intro/Intro'
 import About from './about/About'
 import Work from './work/Work'
+import Contact from './contact/Contact'
 
 function App() {
   return (
     <>
     <Header />
     <Intro />
+
     <div style={{height: "100%"}}>
 
     </div>
+
     <About />
+
     <div style={{height: "100%"}}>
 
     </div>
+
     <Work />
+
+    <div style={{height: "100%"}}>
+
+    </div>
+    
+    <Contact />
     </>
   );
 }
---
+++ src/contact/Contact.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import "./Contact.css"
+
+export default class Contact extends React.Component{
+    render(){
+        return(
+            <div className="contact-container">
+                <div className="top-left-contact-holder">
+                    <div className="contact-title-holder">
+                        <p>&ldquo;I currently reside in Helsinki, Finland and welcome all working opportunities to come.&rdquo;</p>
+                    </div>
+
+                    <div className='mail-holder'>
+                        <div className='mail-img-holder'>
+
+                        </div>
+                        <div className='mail-text-holder'>
+                            <p>minhchinhduong97@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div className='linkedin-holder'>
+                        <div className='linkedin-img-holder'>
+
+                        </div>
+                        <div className='linkedin-text-holder'>
+                            <p>/in/chinh-duong-28679b157/</p>
+                        </div>
+                    </div>
+
+                    <div className='github-holder'>
+                        <div className='github-img-holder'>
+
+                        </div>
+                        <div className='github-text-holder'>
+                            <p>/minhchinh97h</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+}
